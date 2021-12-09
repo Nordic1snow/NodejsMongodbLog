27577c9574c691d1569644e64d99520f3df4720d
hjt <3316114720@qq.com>
2021-12-08 17:50:51 -0800

    install mongoose sucessful

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,9 +1,22 @@
-const { query } = require("express")
 const express =require("express")
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const schema={
+    name:String,
+    age: Number,
+    health:String,
+    hobby:String
+}
+
+const mydata = mongoose.model('cats',schema);
+const kitty = new mydata({ name: 'testZildjian' });
+kitty.save()
+
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
-    
+    console.log(req.query)
 })
 app.listen(10407)
