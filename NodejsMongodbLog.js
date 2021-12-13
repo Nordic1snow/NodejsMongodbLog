17ddcf61921432754643c3f6a0b9fb06313a0cbe
hjt <3316114720@qq.com>
2021-12-12 17:27:27 -0800

    将前台数据写入数据库成功

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express =require("express")
 const app = express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910407');
 
 const schema={
     name:String,
@@ -11,12 +11,14 @@ const schema={
 }
 
 const mydata = mongoose.model('cats',schema);
-const kitty = new mydata({ name: 'testZildjian' });
-kitty.save()
+// const kitty = new mydata({ name: 'testZildjian' });
+// kitty.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({ name: req.query.first,health:req.query.second });
+    kitty.save()
 })
 app.listen(10407)
