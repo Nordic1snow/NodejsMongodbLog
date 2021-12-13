204044d66572361668afd35ca7e1a8dd232f7869
hjt <3316114720@qq.com>
2021-12-12 17:59:33 -0800

    ejs basecall success

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const app = express()
 const mongoose = require('mongoose');
+const ejs = require("ejs")
 mongoose.connect('mongodb://172.21.2.236:27017/190110910407');
 
 const schema={
@@ -16,9 +17,12 @@ const mydata = mongoose.model('cats',schema);
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    // res.send(req.query)
     console.log(req.query)
     const kitty = new mydata({ name: req.query.first,health:req.query.second });
     kitty.save()
+    ejs.renderFile("index.html",{returnVal:"sucess"},(err,str)=>{
+        res.send(str)
+    })
 })
 app.listen(10407)
